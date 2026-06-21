5b6d32235fff1dab1659a593289bc3419ebe5ce0
ucdoganaksesuarlary <ucdogan.com@gmail.com>
2026-06-21 16:03:53 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,11 @@
-// =========================================================================
-// MÜŞDERILERI GOŞÝAN BÖLÜM (DIŇE ŞU ÝERDEN ÜÝTGETMELI, KODA ZEPER ÝETMEZ!)
-// =========================================================================
 const loanData = [
     { 
-        name: "A",          // Ulanyjynyň ady
-        surname: "H***",       // Gizlin familiýasy
-        borrowed: "5,000 TL",  // Alnan karz puly
-        fee: "750 TMT",        // 15% Göterim üstü
-        payback: "5,750 TL",   // Yzyna bermeli jemi pul
-        date: "21.06.2026",    // Alan güni / senesi
-        img: "aman.jpg"        // GitHub-a ýüklän suratyňyzyň ady
-    },
+        name: "Aman",          
+        surname: "H***",       
+        borrowed: "5,000 TMT",  
+        fee: "750 TMT",        
+        payback: "5,750 TMT",   
+        date: "21.06.2026",    
+        img: "aman.jpg"        
+    }
+];
