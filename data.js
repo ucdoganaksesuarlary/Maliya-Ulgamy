90fffd3af69f575487fe7e9fc1b00dd881425f3e
ucdoganaksesuarlary <ucdogan.com@gmail.com>
2026-06-21 16:26:59 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,33 @@
+// =========================================================================
+// MÜŞDERILERI GOŞÝAN BÖLÜM (DIŇE ŞU ÝERDEN ÜÝTGETMELI, KODA ZEPER ÝETMEZ!)
+// =========================================================================
+
 const loanData = [
-    { 
-        name: "Aman",          
-        surname: "H***",       
-        borrowed: "5,000 TL",  
-        fee: "750 TL",        
-        payback: "5,750 TL",   
-        date: "21.06.2026",    
-        img: "aman.jpg"        
+    {
+        name: "A***",            // Ulanyjynyň ady
+        surname: "H***",         // Gizlin familiýasy
+        borrowed: "5,000 TMT",   // Alnan karz puly
+        fee: "750 TMT",          // 15% Göterim üsti
+        payback: "5,750 TMT",    // Yzyna bermeli jemi pul
+        date: "21.06.2026",      // Alan güni / senesi
+        img: "aman.jpg"          // GitHub-a ýüklenen suratynyň ady
+    },
+    {
+        name: "Merdan",
+        surname: "A***",
+        borrowed: "20,000 ₺",
+        fee: "3,000 ₺",
+        payback: "23,000 ₺",
+        date: "Düýn",
+        img: ""                  // Surat ýok bolsa boş galdyryň (M harpy çykar)
+    },
+    {
+        name: "Döwlet",
+        surname: "T***",
+        borrowed: "3,000 TMT",
+        fee: "450 TMT",
+        payback: "3,450 TMT",
+        date: "19.06.2026",
+        img: "dovlet.png"
     }
 ];
